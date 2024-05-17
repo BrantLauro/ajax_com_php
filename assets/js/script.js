f504099050214cb3b027a2752ed17ed22b05bba9
BrantLauro <brantlauro@gmail.com>
2024-05-17 19:13:30 -0300

    nao insere

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,15 @@
-
-$('#form1').submit(function(e){
-    e.preventDefault();
+$('#form1').submit(function(req) {
+    req.preventDefault();
 
     var u_name = $('#name').val();
     var u_comment = $('#comment').val();
 
-    //console.log(u_name, u_comment);
     $.ajax({
-        url: 'http://localhost/PROJS/VIDEO_AULAS/SERIE/03_AJAX_e_PHP/inserir.php',
+        url: 'http://localhost/ajax_com_php/inserir.php',
         method: 'POST',
-        data: {name: u_name, comment:u_comment},
+        data: {name: u_name, comment: u_comment},
         dataType: 'json'
     }).done(function(result){
-        $('#name').val('');
-        $('#comment').val('');
         console.log(result);
-        getComments();
     });
 });
-
-function getComments() {
-    $.ajax({
-        url: 'http://localhost/PROJS/VIDEO_AULAS/SERIE/03_AJAX_e_PHP/selecionar.php',
-        method: 'GET',
-        dataType: 'json'
-    }).done(function(result){
-        console.log(result);
-
-        for (var i = 0; i < result.length; i++) {
-            $('.box_comment').prepend('<div class="b_comm"><h4>' + result[i].name + '</h4><p>' + result[i].comment + '</p></div>');
-        }
-    });
-}
-
-getComments();
--- inserir.php
+++ inserir.php
@@ -1,18 +1,20 @@
 <?php
+
     header('Content-Type: application/json');
 
     $name = $_POST['name'];
     $comment = $_POST['comment'];
 
-    $pdo = new PDO('mysql:host=localhost; dbname=bd-comment-video;', 'root', '');
+    $pdo = new PDO('mysql:host=localhost; dbname=bd_comment;', 'root', '');
 
     $stmt = $pdo->prepare('INSERT INTO comments (name, comment) VALUES (:na, :co)');
     $stmt->bindValue(':na', $name);
     $stmt->bindValue(':co', $comment);
     $stmt->execute();
 
-    if ($stmt->rowCount() >= 1) {
-        echo json_encode('Comentário Salvo com Sucesso');
+    if($stmt->rowCount() >= 1) {
+        echo json_encode('Comentário salvo com Sucesso!');
     } else {
         echo json_encode('Falha ao salvar comentário');
     }
+
